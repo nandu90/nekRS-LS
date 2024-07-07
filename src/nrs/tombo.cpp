d19d89cd615261ee5395d5fd4a5516ddd8e7f201
Kris Rowe <kris.rowe@anl.gov>
2024-07-07 02:53:32 -0500

    Fix nested lambdas which cause performance regression. (#1381)

--- src/nrs/tombo.cpp
+++ src/nrs/tombo.cpp
@@ -16,38 +16,38 @@ occa::memory pressureSolve(nrs_t *nrs, double time, int stage)
     return o_lambda0;
   }();
 
+  const auto o_stressTerm = [&]()
+  {
+    auto o_curl = platform->o_memPool.reserve<dfloat>(nrs->NVfields * nrs->fieldOffset);
+
+    nrs->curlKernel(mesh->Nelements, 1, mesh->o_vgeo, mesh->o_D, nrs->fieldOffset, nrs->o_Ue, o_curl);
+    flopCount += static_cast<double>(mesh->Nelements) * (18 * mesh->Np * mesh->Nq + 36 * mesh->Np);
+
+    oogs::startFinish(o_curl, nrs->NVfields, nrs->fieldOffset, ogsDfloat, ogsAdd, nrs->gsh);
+
+    platform->linAlg->axmyVector(mesh->Nlocal, nrs->fieldOffset, 0, 1.0, nrs->meshV->o_invLMM, o_curl);
+    flopCount += mesh->Nlocal;
+
+    auto o_stressTerm = platform->o_memPool.reserve<dfloat>(nrs->NVfields * nrs->fieldOffset);
+    nrs->curlKernel(mesh->Nelements, 1, mesh->o_vgeo, mesh->o_D, nrs->fieldOffset, o_curl, o_stressTerm);
+    flopCount += static_cast<double>(mesh->Nelements) * (18 * mesh->Np * mesh->Nq + 36 * mesh->Np);
+
+    if (platform->options.compareArgs("VELOCITY STRESSFORMULATION", "TRUE")) {
+      nrs->pressureStressKernel(mesh->Nelements,
+                              mesh->o_vgeo,
+                              mesh->o_D,
+                              nrs->fieldOffset,
+                              nrs->o_mue,
+                              nrs->o_Ue,
+                              nrs->o_div,
+                              o_stressTerm);
+      flopCount += static_cast<double>(mesh->Nelements) * (18 * mesh->Nq * mesh->Np + 100 * mesh->Np);
+    }
+    return o_stressTerm;
+  }();
+
   const auto o_rhs = [&]()
   { 
-    const auto o_stressTerm = [&]()
-    {
-      auto o_curl = platform->o_memPool.reserve<dfloat>(nrs->NVfields * nrs->fieldOffset);
- 
-      nrs->curlKernel(mesh->Nelements, 1, mesh->o_vgeo, mesh->o_D, nrs->fieldOffset, nrs->o_Ue, o_curl);
-      flopCount += static_cast<double>(mesh->Nelements) * (18 * mesh->Np * mesh->Nq + 36 * mesh->Np);
- 
-      oogs::startFinish(o_curl, nrs->NVfields, nrs->fieldOffset, ogsDfloat, ogsAdd, nrs->gsh);
- 
-      platform->linAlg->axmyVector(mesh->Nlocal, nrs->fieldOffset, 0, 1.0, nrs->meshV->o_invLMM, o_curl);
-      flopCount += mesh->Nlocal;
- 
-      auto o_stressTerm = platform->o_memPool.reserve<dfloat>(nrs->NVfields * nrs->fieldOffset);
-      nrs->curlKernel(mesh->Nelements, 1, mesh->o_vgeo, mesh->o_D, nrs->fieldOffset, o_curl, o_stressTerm);
-      flopCount += static_cast<double>(mesh->Nelements) * (18 * mesh->Np * mesh->Nq + 36 * mesh->Np);
-  
-      if (platform->options.compareArgs("VELOCITY STRESSFORMULATION", "TRUE")) {
-        nrs->pressureStressKernel(mesh->Nelements,
-                                mesh->o_vgeo,
-                                mesh->o_D,
-                                nrs->fieldOffset,
-                                nrs->o_mue,
-                                nrs->o_Ue,
-                                nrs->o_div,
-                                o_stressTerm);
-        flopCount += static_cast<double>(mesh->Nelements) * (18 * mesh->Nq * mesh->Np + 100 * mesh->Np);
-      }
-      return o_stressTerm;
-    }();
-
     auto o_gradDiv = platform->o_memPool.reserve<dfloat>(nrs->NVfields * nrs->fieldOffset);
     nrs->gradientVolumeKernel(mesh->Nelements,
                               mesh->o_vgeo,
