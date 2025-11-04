6e770229804e8251122a86acfcc855ac8cd8e593
Yu-Hsiang Lan <lan14@illinois.edu>
2025-11-04 08:25:35 -0600

    Fix mesh_t::interpolate conflict (#1573)

--- src/core/mesh/meshIntp.cpp
+++ src/core/mesh/meshIntp.cpp
@@ -2,15 +2,12 @@
 #include "platform.hpp"
 
 // interpolate to M-points
-occa::memory mesh_t::intpMatrix(std::vector<dfloat> M) 
-{ 
-  nekrsCheck(M.size() > mesh_t::maxNqIntp, 
-             MPI_COMM_SELF, 
-             EXIT_FAILURE, 
+occa::memory mesh_t::intpMatrix(std::vector<dfloat> M)
+{
+  nekrsCheck(M.size() > mesh_t::maxNqIntp,
+             MPI_COMM_SELF,
+             EXIT_FAILURE,
              "target N has to be smaller or equal to %d", mesh_t::maxNqIntp - 1);
-
-  static std::array<occa::memory, mesh_t::maxNqIntp> o_J;
-  if (o_J[M.size() - 1].isInitialized()) return o_J[M.size() - 1];
 
   std::vector<dfloat> J(this->Nq * M.size());
   InterpolationMatrix1D(this->N, this->Nq, this->r, M.size(), M.data(), J.data());
@@ -26,34 +23,52 @@ occa::memory mesh_t::intpMatrix(std::vector<dfloat> M)
     return Jt;
   };
 
-  o_J[M.size() - 1] = platform->device.malloc<dfloat>(J.size());
-  o_J[M.size() - 1].copyFrom((M.size() < this->Nq) ? J.data() : transposeJ().data()); 
+  auto o_J = platform->device.malloc<dfloat>(J.size());
+  o_J.copyFrom((M.size() < this->Nq) ? J.data() : transposeJ().data());
 
-  return o_J[M.size() - 1];
+  return o_J;
 }
 
 void mesh_t::interpolate(const occa::memory& o_z, mesh_t *mesh, occa::memory& o_zM, bool uniform, dlong nel_)
 {
   auto nel = (nel_ > 0) ? nel_ : this->Nelements;
 
-  if (uniform) {
-    auto M = [&]()
-    {
-      std::vector<dfloat> r(mesh->N + 1);
-      r[0] = -1.0;
-      r[mesh->N] = 1.0;
-  
-      const auto dr = (r[mesh->N] - r[0]) / mesh->N;
-      for(int i = 1; i < mesh->N; i++) r[i] = r[i-1] + dr;
-      return r;
-    }();
+  nekrsCheck(mesh->Nq > mesh_t::maxNqIntp,
+             MPI_COMM_SELF,
+             EXIT_FAILURE,
+             "target N has to be smaller or equal to %d", mesh_t::maxNqIntp - 1);
 
-    this->intpKernel[mesh->N](nel, intpMatrix(M), o_z, o_zM);
-    return;
+  if (uniform) {
+
+    static std::array<occa::memory, mesh_t::maxNqIntp> o_Juni;
+    if (!o_Juni[mesh->N].isInitialized()) {
+
+      auto M = [&]()
+      {
+        std::vector<dfloat> r(mesh->N + 1);
+        r[0] = -1.0;
+        r[mesh->N] = 1.0;
+
+        const auto dr = (r[mesh->N] - r[0]) / mesh->N;
+        for(int i = 1; i < mesh->N; i++) r[i] = r[i-1] + dr;
+        return r;
+      }();
+
+      o_Juni[mesh->N] = intpMatrix(M);
+    }
+
+    this->intpKernel[mesh->N](nel, o_Juni[mesh->N], o_z, o_zM);
+
+  } else {
+
+    static std::array<occa::memory, mesh_t::maxNqIntp> o_Jgll;
+    if (!o_Jgll[mesh->N].isInitialized()) {
+      std::vector<dfloat> M(mesh->Nq);
+      for(int i = 0; i < M.size(); i++) M[i] = mesh->r[i];
+      o_Jgll[mesh->N] = intpMatrix(M);
+    }
+
+    this->intpKernel[mesh->N](nel, o_Jgll[mesh->N], o_z, o_zM);
   }
 
-  std::vector<dfloat> M(mesh->Nq);
-  for(int i = 0; i < M.size(); i++) M[i] = mesh->r[i];
-
-  this->intpKernel[mesh->N](nel, intpMatrix(M), o_z, o_zM);
 }
